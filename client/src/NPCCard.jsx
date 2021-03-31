8b3cdd496912d4a6726edaea8aca5bf5ba11794e
Austin Testut <austintestut@gmail.com>
2021-03-30 17:53:35 -0700

    created skeleton for NPC cards within container

---
+++ client/src/NPCCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const NPCCard = (/* props */) => {
+  return (
+    <div>
+      <div>Name: {/* add name from props here */}</div>
+      <div>Race: {/* add Race from props here */}</div>
+      <div>Demeanor: {/* add Demeanor from props here */}</div>
+    </div>
+  )
+};
+
+export default NPCCard;
--- client/src/NPCCardContainer.jsx
+++ client/src/NPCCardContainer.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import styled from 'styled-components';
 
 const NPCCardContainer = (/* props */) => {
-  // for NPC in DB:
+  // for each NPC in DB:
   return (
     <div>
-      <div>Name: {/* add name from props here */}</div>
-      <div>Race: {/* add Race from props here */}</div>
-      <div>Demeanor: {/* add Demeanor from props here */}</div>
+      <NPCCard />
     </div>
   )
 };
