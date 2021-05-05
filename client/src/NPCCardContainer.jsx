46775bb31c521bb66b105161d17039a1a902f99e
Austin Testut <austintestut@gmail.com>
2021-05-05 13:48:35 -0500

    removed styled components from CSS styling

--- client/src/NPCCardContainer.jsx
+++ client/src/NPCCardContainer.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
 import NPCCard from './NPCCard';
 
 const NPCCardContainer = ({ npcData, showEditForm }) => {
   return (
-    <StyledCardContainer>
+    <div id="card-container">
     {npcData.map((npc) => {
       return <NPCCard key={npc.name} npc={npc} showEditForm={showEditForm}/>
     })}
-    </StyledCardContainer>
+    </div>
   )};
 
 export default NPCCardContainer;
 
-const StyledCardContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  padding: 0 5% 0 5%;
-`;
