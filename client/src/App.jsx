a31205730bd55992dffc600a71dd62f24ad396a3
Austin Testut <austintestut@gmail.com>
2021-05-05 13:49:44 -0500

    removed styled component import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from 'styled-components';
 import axios from 'axios';
 import AddNewNPCButton from './AddNewNPCButton';
 import AddNewNPCForm from './AddNewNPCForm';
