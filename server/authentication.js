96b3e0af6b674f4b02cbeb2d3ee7dcbfbeec052a
Austin Testut <austintestut@gmail.com>
2021-05-11 14:38:24 -0500

    added authentication functions

---
+++ server/authentication.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+require('./passport-setup.js');
+
+const authScope = passport.authenticate('google', {
+  scope: ['email', 'profile'],
+});
+const googleAuth = passport.authenticate('google');
+const loggedinRedirect = (req, res) => {
+  res.redirect('/loggedin');
+};
+const sendUser = (req, res) => {
+  res.send({ userName: req.user.userName, _id: req.user._id });
+};
+const homeRedirect = (req, res) => {
+  res.redirect('/');
+};
+const logout = (req, res) => {
+  req.logout();
+  req.session = null;
+  res.redirect('/');
+};
+const authCheck = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/');
+  }
+};
+
+module.exports = {
+  authScope,
+  googleAuth,
+  loggedinRedirect,
+  sendUser,
+  homeRedirect,
+  logout,
+  authCheck,
+};
