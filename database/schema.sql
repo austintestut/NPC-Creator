c00b279b7292767dca73ec28f214fb62e0f3c853
Austin Testut <austintestut@gmail.com>
2021-05-11 16:05:46 -0500

    updated schema to provide for foreign keys and multiple tables in preparation for authentication implementation

--- database/schema.sql
+++ database/schema.sql
@@ -1,18 +1,30 @@
- \c postgres;
+\c postgres;
 
- DROP DATABASE IF EXISTS npcs;
+DROP DATABASE IF EXISTS npcs;
 
 CREATE DATABASE npcs;
 
 \c npcs;
 
 CREATE TABLE allNPCs (
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
+  owner_id INT,
   name VARCHAR(50),
   race VARCHAR(50),
   demeanor VARCHAR(50),
   quality VARCHAR,
   notes VARCHAR
+);
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  user_name VARCHAR(75)
+);
+
+CREATE TABLE users_npcs (
+  id SERIAL,
+  user_id INT REFERENCES users,
+  npc_id INT REFERENCES allNPCs
 );
 
 -- Must be postgres user with SUPERUSER privileges (try: $ su postgres):
