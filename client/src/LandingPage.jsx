54d0d3df89a3224f0a8133699ad16fa8bee7eaca
Austin Testut <austintestut@gmail.com>
2021-05-13 11:20:09 -0500

    added anchor button for google login

---
+++ client/src/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LandingPage = ({ authenticateUser }) => (
+  <div>
+    Landing Page to go here
+    <a href="/google">
+      <button type="button" id="LoginButton">
+        Log in with Google
+      </button>
+    </a>
+  </div>
+);
+
+export default LandingPage;
