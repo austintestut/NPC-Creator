71acc7df0c4b491bc438da56e5d2cfc62d39298f
Austin Testut <austintestut@gmail.com>
2021-05-14 15:40:02 -0500

    grammar fix "its"

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => (
   <footer>
     <span>
-      This application is being constantly updated and is in no way in it's
+      This application is being constantly updated and is in no way in its
       final state. This application uses cookies for authenticating user
       sessions. By using this application you agree to use of cookies in this
       fashion.
