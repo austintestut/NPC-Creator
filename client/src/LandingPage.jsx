a9b212d2d1754b7a2601108002ec3a485f4e2abd
Austin Testut <austintestut@gmail.com>
2021-05-20 13:18:59 -0500

    login button styling

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -6,7 +6,13 @@ const LandingPage = ({ authenticateUser }) => (
     <span id="landing-title">NPC Creator</span>
     <a href="/google">
       <button type="button" id="login-button">
-        Log in with Google
+        <div className="google-icon-wrapper">
+          <img
+            className="google-icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+          />
+        </div>
+        <span className="login-text">Log in with Google</span>
       </button>
     </a>
     <Footer />
