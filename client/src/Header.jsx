96ac999ec6cbbdeb10160cfec8af881eb7fe380e
Zoey Brennan <zoeybrennan@Zoeys-MacBook-Pro.local>
2021-05-26 00:49:16 -0500

    added header buffer for spacing

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -4,6 +4,7 @@ import Title from "../../public/images/npc-text.png";
 const Header = ({userName}) => (
   <div id="header">
     <img className="app-logo" src={Title} />
+    <div id="header-buffer" />
     <span id="app-username">{userName}</span>
     <a href="/logout">
       <button id="logout-button">Logout</button>
