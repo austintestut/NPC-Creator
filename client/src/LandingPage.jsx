f40dfdfdf71c1c7eb946175c126b4f9fbfdc4e61
Austin Testut <austintestut@gmail.com>
2021-06-06 17:37:37 -0500

    styled login buttons on landing page

--- client/src/LandingPage.jsx
+++ client/src/LandingPage.jsx
@@ -5,19 +5,26 @@ import Title from "../../public/images/npcc.png";
 const LandingPage = ({ authenticateUser, makeSessionless }) => (
   <div id="landing">
     <img id="landing-title-pic" src={Title} />
-    <span id="landing-description">Create and save new Non-Player Characters on the fly!</span>
-    <a href="/google">
-      <button type="button" id="login-button">
-        <div className="google-icon-wrapper">
-          <img
-            className="google-icon"
-            src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-          />
-        </div>
-        <span className="login-text">Log in with Google</span>
+    <span id="landing-description">
+      Create and save new Non-Player Characters on the fly!
+    </span>
+    <div className="login-button-container">
+        <a href="/google" className="login-anchor">
+          <button type="button" id="login-button">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+              />
+            </div>
+            <span className="login-text">Log in with Google</span>
+          </button>
+        </a>
+      <span id="login-separator">- or -</span>
+      <button id="sessionless-login" onClick={makeSessionless}>
+        Continue without an account
       </button>
-    </a>
-      <button id="sessionless-login" onClick={makeSessionless}>Continue without an account</button>
+    </div>
     <Footer />
   </div>
 );
