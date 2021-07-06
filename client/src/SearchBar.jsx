4b5a162d52bc14d702b5e645323e7133e3ed77f9
Austin Testut <austintestut@gmail.com>
2021-07-06 11:06:18 -0500

    added search value id to input for DOM targeting

--- client/src/SearchBar.jsx
+++ client/src/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SearchBar = () => (
   <form id="search">
     <label hidden>Search </label>
-    <input type="text" placeholder="Search NPCs"></input>
+    <input type="text" placeholder="Search NPCs" id="searchValue"></input>
   </form>
 );
 
